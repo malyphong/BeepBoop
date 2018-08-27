4373061190bc4ea0b79f9382bc5eab64d13cacf4
Maly Phongsavanh <phongsavanh619@icloud.com>
2018-08-26 17:53:24 -0700

    pushes BEEP and im sorry dave into array

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
-var zero = "Beep!";
-var phrase = "I'm sorry, Dave. I cannot do that";
 var numberArray = [];
 
-
- var result = function(input) {
-   for(var index = 0; index <= input; index ++)
-    (numberArray).push(index);
-    if (input % 3 === 0) {
-      return phrase;
-    } else if (numberArray.toString().includes('1')) {
-      numberArray.push("Boop!")
-      console.log(numberArray);
-    } else if (numberArray === 0) {
-      return zero;
+ var result = function(userInput) {
+   var newArray = [];
+   for(var index = 0; index <= userInput; index ++)
+    if (index % 3 === 0 && index > 0) {
+      newArray.push("I'm sorry, Dave. I cannot do that.");
+      console.log(newArray);
+    } else if (index === 1 || numberArray.toString().includes('1')) {
+      newArray.push("Boop!")
+      console.log(newArray);
   }
+  return newArray;
 }
 $(document).ready(function() {
   $("form.form-class").submit(function(event) {
