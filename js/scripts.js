ca725b5a00355cb0c64ca491335d4c83842be038
Maly Phongsavanh <phongsavanh619@icloud.com>
2018-08-26 19:19:24 -0700

    add styling to the HTML also hide/show output div

--- js/scripts.js
+++ js/scripts.js
@@ -7,23 +7,22 @@ var numberArray = [];
       newArray.push("I'm sorry, Dave but I cannot do that");
     } else if (index === 0 || numberArray.toString().includes("0")) {
       newArray.push("Beep");
-    } else if (index >= 1 || numberArray.toString().includes("1")) {
-      // >= 1 changes all integers//
+    } else if (index === 1 || numberArray.toString().includes("1")) {
       newArray.push("Boop")
-      // 10 and 20 are still not changed"
     } else {
       newArray.push(index);
     }
-
   return newArray.join("! ");
 }
 $(document).ready(function() {
   $("form.form-class").submit(function(event) {
     event.preventDefault();
+    $(".output").show();
 
    var input = parseInt($(".inputNumber").val());
    var resultOutput = result(input);
 
      $("#output").text(resultOutput);
+     $(".reverseOutput").text(resultOutput)
    });
 });
