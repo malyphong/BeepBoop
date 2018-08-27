0feaf2c34d159f8321b208e8989ada4e661c468a
Maly Phongsavanh <phongsavanh619@icloud.com>
2018-08-26 18:47:01 -0700

    change all integers to beep

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,18 @@ var numberArray = [];
    var newArray = [];
    for(var index = 0; index <= userInput; index ++)
     if (index % 3 === 0 && index > 0) {
-      newArray.push("I'm sorry, Dave. I cannot do that!");
-    } else if (index === 1 || numberArray.toString().includes("1")) {
-      newArray.push("Boop!")
+      newArray.push("I'm sorry, Dave but I cannot do that");
     } else if (index === 0 || numberArray.toString().includes("0")) {
-      newArray.push("Beep!");
+      newArray.push("Beep");
+    } else if (index >= 1 || numberArray.toString().includes("1")) {
+      // >= 1 changes all integers//
+      newArray.push("Boop")
       // 10 and 20 are still not changed"
     } else {
       newArray.push(index);
     }
 
-  return newArray.join(", ");
+  return newArray.join("! ");
 }
 $(document).ready(function() {
   $("form.form-class").submit(function(event) {
